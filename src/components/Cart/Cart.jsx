b44f62dd0ccbc8af9d3fd4fabefc23ea2edf4090
Aaditya8683 <64246876+Aaditya8683@users.noreply.github.com>
2023-08-24 15:19:10 +0530

    update cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,14 @@ import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import CartItem from "./CartItem/CartItem";
 
+import { useNavigate } from "react-router-dom";
 import "./Cart.scss";
 import { useContext } from "react";
 import { Context } from "../../utils/context";
 import {loadStripe} from "@stripe/stripe-js"
 import { makePaymentRequest } from "../../utils/api";
 const Cart = ({ setShowCart }) => {
+    const navigate = useNavigate();
     const { cartItems, cartSubtotal } = useContext(Context);
 
     const stripePromise = loadStripe(
@@ -42,7 +44,7 @@ const Cart = ({ setShowCart }) => {
                 {!cartItems?.length && <div className="empty-cart">
                     <BsCartX />
                     <span>No products in the cart.</span>
-                    <button className="return-cta">RRTURN TO SHOP</button>
+                    <button className="return-cta" onClick={() => {navigate("/")}}>RETURN TO SHOP</button>
                 </div>}
 
                 {!!cartItems?.length &&<>
